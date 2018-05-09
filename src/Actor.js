2cf3403659bcaa424492d55624d4fba6b1f348fb
Michael Clayton <michael.clayton@procore.com>
2018-05-09 13:50:40 -0700

    More cleanup

--- src/Actor.js
+++ src/Actor.js
@@ -1,42 +1,37 @@
 import React, { Component } from 'react';
+import cx from 'classnames';
 
 class Actor extends Component {
-  constructor(props) {
-    super(props);
-    this.onAdance = props.onAdance;
-    this.state = props.actor;
-  }
-
   getTotal = () => {
-    return this.state.roll + this.state.initiative;
+    const { actor } = this.props;
+    return actor.roll + actor.initiative;
   };
 
   showMath = () => {
-    return this.state.roll + " + " + this.state.initiative;
-  };
-  
-  backgroundClass = () => {
-    if (this.state.active) {
-      return "background-secondary";
-    } else {
-      return "";
-    }
+    const { actor } = this.props;
+    return `${actor.roll} + ${actor.initiative}`;
   };
 
   handleClick = (e) => {
-    if (this.props.actor.active) {
-      this.props.onAdvance(e);
+    const { actor, onAdvance } = this.props;
+    if (actor.active) {
+      onAdvance(e);
     }
   };
 
   render() {
+    const { actor } = this.props;
+    const containerClasses = cx('sm-4', 'row', 'flex-edges', {
+      'background-secondary': actor.active
+    });
+
     return (
       <div
-        className={`sm-4 row flex-edges ${this.backgroundClass()}`}
+        className={containerClasses}
         onClick={this.handleClick}
       >
         <div className="col">
-          <p className="margin-none">{this.state.name}</p>
+          <p className="margin-none">{actor.name}</p>
         </div>
         <div className="col">
           <p className="margin-none badge" title={this.showMath()}>{this.getTotal()}</p>
